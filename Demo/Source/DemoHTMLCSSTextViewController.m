f34927c9302a27e17d08e45f6e19b96850428040
HuiHuang <huanghui3@wps.cn>
2017-08-20 22:23:58 +0800

    简化CSS文件

--- Demo/Source/DemoHTMLCSSTextViewController.m
+++ Demo/Source/DemoHTMLCSSTextViewController.m
@@ -15,6 +15,10 @@
 
 @interface DemoHTMLCSSTextViewController () <DTAttributedTextContentViewDelegate>
 
+@property (nonatomic, weak) UIView *originView;
+@property (nonatomic, strong) UIView *snapshotView;
+@property (nonatomic, strong) UIView *maskView;
+
 @end
 
 @implementation DemoHTMLCSSTextViewController
@@ -66,14 +70,33 @@
 	{
 
 		UIImageView *imageView = [[UIImageView alloc] initWithFrame:frame];
+		imageView.userInteractionEnabled = YES;
+
 		[imageView sd_setImageWithURL:attachment.contentURL
 					 placeholderImage:[UIImage imageNamed:@"Oliver@2x.jpg"]
 							completed:nil];
+
+		UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapImageAction:)];
+		[imageView addGestureRecognizer:tap];
 
 		return imageView;
 	}
 
 	return nil;
+}
+
+- (void)tapImageAction:(UITapGestureRecognizer *)gesture {
+
+	UIView *originView = gesture.view;
+
+	[UIView animateWithDuration:0.3 animations:^{
+		originView.transform = CGAffineTransformScale(CGAffineTransformIdentity, 1.5, 1.5);
+	} completion:^(BOOL finished) {
+		[UIView animateWithDuration:0.4 animations:^{
+			originView.transform = CGAffineTransformIdentity;
+		}];
+	}];
+	
 }
 
 - (UIView *)attributedTextContentView:(DTAttributedTextContentView *)attributedTextContentView
@@ -85,19 +108,22 @@
 	NSString *identifier = [attributes objectForKey:DTGUIDAttribute];
 
 	DTLinkButton *button = [[DTLinkButton alloc] initWithFrame:frame];
-	[button addTarget:self action:@selector(openURL) forControlEvents:UIControlEventTouchUpInside];
+	button.GUID = identifier;
+	button.URL = URL;
+	[button addTarget:self action:@selector(openURL:) forControlEvents:UIControlEventTouchUpInside];
 
 	UIImage *normalImage = [attributedTextContentView contentImageWithBounds:frame options:DTCoreTextLayoutFrameDrawingDefault];
 	[button setImage:normalImage forState:UIControlStateNormal];
 	UIImage *highlightImage = [attributedTextContentView contentImageWithBounds:frame options:DTCoreTextLayoutFrameDrawingDrawLinksHighlighted];
 	[button setImage:highlightImage forState:UIControlStateHighlighted];
+	[button setImage:highlightImage forState:UIControlStateSelected];
 
 	return button;
 }
 
-- (void)openURL {
+- (void)openURL:(DTLinkButton *)button {
 
-	NSLog(@"open url");
+	[[UIApplication sharedApplication] openURL:button.URL];
 }
 
 //- (BOOL)attributedTextContentView:(DTAttributedTextContentView *)attributedTextContentView shouldDrawBackgroundForTextBlock:(DTTextBlock *)textBlock frame:(CGRect)frame context:(CGContextRef)context forLayoutFrame:(DTCoreTextLayoutFrame *)layoutFrame
@@ -164,8 +190,8 @@
 	NSMutableDictionary *options = @{
 									 DTIgnoreInlineStylesOption: @NO,
 									 DTDefaultLinkDecoration: @NO,
-									 DTDefaultLinkColor: @"#888888",
-									 DTLinkHighlightColorAttribute: @"#666666",
+									 DTDefaultLinkColor: @"#00FF00",
+									 DTLinkHighlightColorAttribute: @"#0000FF",
 									 DTDefaultFontSize: @15,
 									 DTDefaultFontFamily: @"Times New Roman",
 									 DTDefaultFontName: @"HelveticaNeue-Light",
